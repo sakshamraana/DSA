6f6f04a63f6331dc544e23a35ef1cded80393e9e
Saksham Rana <ranasaksham83@gmail.com>
2026-07-02 01:43:58 +0530

    Time: 1 ms (100%), Space: 77.3 MB (16.88%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int[]freq=new int[nums.length+2];
+        for(int num:nums){
+            if(num>0 && num<=nums.length){
+            freq[num]++;
+            }
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(freq[i]==0){
+                return i;
+            }
+        }
+        return nums.length+1;
+    }
+}
