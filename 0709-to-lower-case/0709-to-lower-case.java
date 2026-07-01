d0742fb52fb2a53d56fbdeaa2d21ae483f8536fd
Saksham Rana <ranasaksham83@gmail.com>
2026-07-01 11:31:15 +0530

    Time: 1 ms (26.63%), Space: 43.2 MB (19.35%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sc=new StringBuilder();
+        s=s.toLowerCase();
+        sc.append(s);
+        return sc.toString();
+    }
+}
