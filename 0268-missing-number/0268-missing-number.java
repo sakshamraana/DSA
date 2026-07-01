adf572f041351c3f18d16da98540fa4e68de01bd
Saksham Rana <ranasaksham83@gmail.com>
2026-07-02 01:34:26 +0530

    Time: 0 ms (100%), Space: 46.6 MB (99.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int[]freq=new int[nums.length+1];
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(int i=0;i<=nums.length;i++){
+            if(freq[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
